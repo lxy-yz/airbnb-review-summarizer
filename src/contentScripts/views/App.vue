431b0fced996bdf1b4b50ea79ea52f5811bcebae
lxy-yz <lxy.dev.yz@gmail.com>
2023-05-25 14:11:23 -0700

    Going

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -3,6 +3,50 @@ import { useToggle } from '@vueuse/core'
 import 'uno.css'
 
 const [show, toggle] = useToggle(false)
+
+const LISTING_REVIEWS_REGEX = /airbnb\.com\/rooms\/\d+/i
+const defaultEnabled = LISTING_REVIEWS_REGEX.test(window.location.href)
+const [enabled, setEnabled] = useToggle(true)
+
+const summary = ref('')
+const OPENAI_API_KEY = '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'
+const makePrompt = (x: string) => `
+I am an avid traveler who likes to stay in Airbnb.  Help me to summarize the airbnb listing reviews below make informed decision about whether this listing is a fit for me or not. A few things that I care about
+- Five star reviews
+- Good sleep environment
+- Safe neighborhood
+- Convenient location
+
+Listing reviews below: 
+${x}
+`
+async function summarizeReviews(reviews: string) {
+  const res = await fetch('https://api.openai.com/v1/chat/completions', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${OPENAI_API_KEY}`,
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a helpful assistant that accurately answers queries using Allen\'s essays. Use the text provided to form your answer, but avoid copying word-for-word from the essays. Try to use your own words when possible. Keep your answer under 5 sentences. Be accurate, helpful, concise, and clear.',
+        },
+        {
+          role: 'user',
+          content: makePrompt(reviews),
+        },
+      ],
+      max_tokens: 150,
+      temperature: 0.0,
+      // stream: true
+    }),
+  })
+  if (res.ok)
+    summary.value = (await res.json()).choices[0].message.content
+}
 </script>
 
 <template>
@@ -18,6 +62,20 @@ const [show, toggle] = useToggle(false)
         Vitesse WebExt
       </h1>
       <SharedSubtitle />
+
+      <div
+        :show="enabled"
+      >
+        cool shit
+      </div>
+      <button
+        @click="summarizeReviews('this place is amazing, safe, clean, good located')"
+      >
+        click me
+      </button>
+      <p>
+        {{ summary }}
+      </p>
     </div>
     <button
       class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none"
