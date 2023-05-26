8578176582f3383d255caf34108bc9f82a26880d
lxy-yz <lxy.dev.yz@gmail.com>
2023-05-26 16:18:36 -0700

    style: unocss form

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'unocss/vite'
 import { presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
+import { presetForms } from '@julr/unocss-preset-forms'
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
     presetIcons(),
+    presetForms(),
   ],
   transformers: [
     transformerDirectives(),
