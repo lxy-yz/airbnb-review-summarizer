1145e0b7d3d773388b84040eefe608ae779e5f14
Allen <90570853+lxy-yz@users.noreply.github.com>
2023-06-01 12:53:00 -0700

    Update footer.tsx

--- website/components/footer.tsx
+++ website/components/footer.tsx
@@ -5,7 +5,12 @@ export default function Footer() {
   return (
     <footer className="">
       <div className="py-6 w-full flex justify-center">
-        <a href="https://www.producthunt.com/posts/airbnb-review-summarizer?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-airbnb&#0045;review&#0045;summarizer" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=397254&theme=light" alt="Airbnb&#0032;Review&#0032;Summarizer - Summarize&#0032;listing&#0032;reviews&#0032;to&#0032;be&#0032;useful | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+        <a 
+          href="https://www.producthunt.com/posts/airbnb-review-summarizer?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-airbnb&#0045;review&#0045;summarizer" 
+          target="_blank"
+        >
+          <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=397254&theme=light" alt="Airbnb&#0032;Review&#0032;Summarizer - Summarize&#0032;listing&#0032;reviews&#0032;to&#0032;be&#0032;useful | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" />
+        </a>
       </div>
       <div className="relative">
         <a
